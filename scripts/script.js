73178c340cdb2a49577e93cb94f4b1f1f2dbe76f
John Adeniran <jasolajohn@gmail.com>
2020-06-21 21:23:28 +0100

    git init

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const addInput = document.getElementById('add');
+const inputs = document.getElementById('inputs');
+
+
+
+
+
+
+addInput.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("clicked");
+
+})
